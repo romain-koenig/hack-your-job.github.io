43db8eaf62a1f61d7591a8fafcec280115ce4f01
Gilles Figuière <me@gillesfiguiere.com>
2019-06-21 13:37:58 +0200

    Moved from profile's database in JS object file linked to HTML to JSON loaded asynchonously

--- js/vue-app.js
+++ js/vue-app.js
@@ -1,14 +1,60 @@
-'use strict';
+'use strict'
+
+const getMembers = function() {
+
+  const vueData = this
+
+  fetch('profiles-data/job-hackers.json').then(response => {
+    response.json().then(json => {
+      vueData.members = json
+
+      const cities = {}
+
+      const skills = {}
+
+      vueData.members.forEach(member => {
+
+        member.cities.forEach(city => {
+          if (cities.hasOwnProperty(city)) {
+            cities[city]++
+          } else {
+            cities[city] = 1
+          }
+        })
+
+        member.skills.forEach(skill => {
+          if (skills.hasOwnProperty(skill)) {
+            skills[skill]++
+          } else {
+            skills[skill] = 1
+          }
+        })
+
+        member.mainCity = member.cities[0]
+
+      })
+
+      for (const city in cities) {
+        vueData.cities.push({ title: city, members: cities[city] })
+      }
+
+      for (const skill in skills) {
+        vueData.skills.push({ title: skill, members: skills[skill] })
+      }
+    })
+  })
+}
 
 new Vue({
   el: '#app-vue',
+  mounted: getMembers,
   data: {
     filtering: '',
     selectedCity: null,
-    cities: data.cities,
-    skills: data.skills,
-    members: data.members,
-    membersFilteredCount: data.members.length,
+    cities: [],
+    skills: [],
+    members: [],
+    membersFilteredCount: 0,
     contact: {
       success: false,
       error: null,
@@ -19,21 +65,21 @@ new Vue({
     }
   },
   computed: {
-    displayedMembers: function(){
+    displayedMembers: function () {
       var filtered = this.filtered(this.members);
-      return filtered.sort(function(member1, member2){
+      return filtered.sort(function (member1, member2) {
         return member1.name.localeCompare(member2.name);
       })
     }
   },
   methods: {
-    filtered: function(members) {
+    filtered: function (members) {
       if (this.filtering === '' && !this.selectedCity) {
         this.membersFilteredCount = members.length;
         return members;
       }
       var filter = this.filtering.toLowerCase();
-      var filteredList = members.filter(function(member) {
+      var filteredList = members.filter(function (member) {
         if (this.selectedCity && member.cities.indexOf(this.selectedCity) === -1) {
           return false;
         }
